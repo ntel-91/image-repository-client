60c3b98a291b494dcf5d2cc5f75752fe007063a8
ntel-91 <ntelenson@gmail.com>
2020-09-04 11:47:50 -0600

    moved NavBar component

--- src/App.js
+++ src/App.js
@@ -8,9 +8,6 @@ import SignUpForm from './components/SignUpForm';
 import LoginForm from './components/LoginForm';
 
 const App = (props) => {
-    const [visible, setVisible] = useState(false);
-    const [images, setImages] = useState([]);
-    const [selectedImages, setSelectedImages] = useState([]);
     const [currentUser, setCurrentUser] = useState(null);
     
     useEffect(() => {
@@ -43,72 +40,16 @@ const App = (props) => {
         props.history.push("/login")
     }
 
-    const showSideBar = () => {
-        setVisible(!visible);
-    }
-
-    const uploadPhoto = (fileData) => {
-        const formData = new FormData();
-        formData.append("file", fileData)
-
-        fetch('http://localhost:3000/api/v1/items', {
-            method: "POST",
-            body: formData
-        })
-        .then(res => res.json())
-        .then(data => {
-            debugger
-            setImages(data);
-        })
-    }
-
-    const selectImages = (imageId) => {
-        const images = [...selectedImages, imageId];
-        setSelectedImages(images);
-    }
-
-    const deleteImages = (photos) => {
-        const photoId = photos[0]
-        fetch(`http://localhost:3000/api/v1/items/${photoId}`,{
-            method: 'DELETE'
-        })
-        .then(res => res.json())
-        .then(res => {
-            if (res.errors) {
-                alert(res.errors)
-            } else {
-                const updatedImages = images.filter(i => i.id !== photoId);
-                setImages(updatedImages);
-            }
-            // setImages(data)
-        })
-    }
-
     return (
         <Grid>
-            { currentUser ?
-                <Grid.Row>
-                    <NavBar 
-                        showSideBar={showSideBar}
-                        uploadPhoto={uploadPhoto}
-                        selectedImages={selectedImages}
-                        deleteImages={deleteImages}
-                        logOut={logOut}
-                    />
-                </Grid.Row>
-            :
-                null
-            }
             <Switch>
                 <Grid.Row centered>
                     <Route path="/images" render={(routerProps) => {
                         return <ImageContainer 
-                            isVisible={visible} 
-                            showSideBar={showSideBar}
+                            setUser={setUser}
                             currentUser={currentUser}
-                            images={images}
-                            selectImages={selectImages}
-                            setImages={setImages}
+                            {...routerProps}
+                            logOut={logOut}
                         />
                     }} />
                     <Route path="/signup" render={(routerProps) => {
--- src/containers/ImageContainer.js
+++ src/containers/ImageContainer.js
@@ -1,15 +1,19 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import Item from '../components/Item';
+import NavBar from '../components/NavBar';
 import { 
     Container, 
     Grid, 
     Icon, 
     Menu,
-    Segment,
-    Sidebar 
+    Sidebar
 } from 'semantic-ui-react';
 
-const ImageContainer = ({ isVisible, showSideBar, images, currentUser, selectImages, setImages }) => {
+const ImageContainer = ({ currentUser, logOut }) => {
+
+    const [visible, setVisible] = useState(false);
+    const [images, setImages] = useState([]);
+    const [selectedImages, setSelectedImages] = useState([]);
 
     useEffect(() => {
         fetch(`http://localhost:3000/api/v1/users/${currentUser.id}/items`)
@@ -19,8 +23,54 @@ const ImageContainer = ({ isVisible, showSideBar, images, currentUser, selectIma
         })
     }, [])
 
+    const uploadPhoto = (fileData) => {
+        const formData = new FormData();
+        formData.append("file", fileData)
+
+        fetch('http://localhost:3000/api/v1/items', {
+            method: "POST",
+            body: formData
+        })
+        .then(res => res.json())
+        .then(data => {
+            setImages(data);
+        })
+    }
+
+    const showSideBar = () => {
+        setVisible(!visible);
+    }
+
+    const selectImages = (imageId) => {
+        const images = [...selectedImages, imageId];
+        setSelectedImages(images);
+    }
+
+    const deleteImages = (photos) => {
+        const photoId = photos[0]
+        fetch(`http://localhost:3000/api/v1/items/${photoId}`,{
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(res => {
+            if (res.errors) {
+                alert(res.errors)
+            } else {
+                const updatedImages = images.filter(i => i.id !== photoId);
+                setImages(updatedImages);
+            }
+        })
+    }
+
     return (
-        <Grid columns={1}>
+        <Grid>
+            <NavBar
+                showSideBar={showSideBar}
+                uploadPhoto={uploadPhoto}
+                selectedImages={selectedImages}
+                deleteImages={deleteImages}
+                logOut={logOut}
+            />
             <Sidebar
                 as={Menu}
                 animation='overlay'
@@ -28,7 +78,7 @@ const ImageContainer = ({ isVisible, showSideBar, images, currentUser, selectIma
                 inverted
                 onHide={() => showSideBar()}
                 vertical
-                visible={isVisible}
+                visible={visible}
                 width='thin'
             >
                 <Menu.Item as='a'>
